3f33e4b234da3ad3fefaefdcb1d0f145b3a3f155
amarmotghare <60289520+amarmotghare@users.noreply.github.com>
2020-11-08 16:01:49 +0530

    Update code.js

--- code.js
+++ code.js
@@ -12,7 +12,7 @@ let songList = [
 		artistname:"Silent Partner"
 	},
 	{
-		thumbnail:"science main.jpg",
+		thumbnail:"bio.jpg",
 		audio:"Bovi.mp3",
 		songname:"Bovi",
 		artistname:"The Grand Affair",
